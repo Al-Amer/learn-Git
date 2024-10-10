c50e3ce2e41a0bcce266ef3e765b2e9d6281b6be
ameralmunajjed <a.munajed@icloud.com>
2024-10-10 19:39:19 +0200

    add info.txt and info2.txt and she.sh

---
+++ she.sh
@@ -0,0 +1,14 @@
+echo "Hello"
+echo " this is ls "
+ls 
+echo "-------------"
+echo "create file info2.txt"
+ls -al > info2.txt
+echo "-------------"
+echo "read info2.txt file"
+cat info2.txt
+echo "-------------"
+echo "read file info.txt "
+cat info.txt
+echo "-------------"
+echo " bye"
